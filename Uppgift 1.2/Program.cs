0759a52bdf4f68f60679fb1cf99796865e172625
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:13:21 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            //Ändra bakgrundsfärg till blå
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            //Ändra textfärgen till vit
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Your PC ran into a problem that it couldn't handle, and now it needs to restart.");
+            Console.ReadKey();
+        }
+    }
+}
+   
